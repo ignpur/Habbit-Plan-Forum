069f801a58f882ab65f8b17c66d8de9ba79e0c51
IgnPur <100840309+ignpur@users.noreply.github.com>
2024-11-21 23:26:23 +0200

    Update AplicationDbContext.cs

--- HabitPlanForum.Server/Data/AplicationDbContext.cs
+++ HabitPlanForum.Server/Data/AplicationDbContext.cs
@@ -1,20 +1,25 @@
 ﻿using HabitPlanForum.Server.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using HabitPlanForum.Server.Auth.Model;
+
 
 namespace HabitPlanForum.Server.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ForumUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        : base(options)
         {
         }
         //private readonly IConfiguration _configuration;
         public DbSet<Topic> Topics { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Session> Sessions { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             // Topic -> Posts (Cascade delete)
             modelBuilder.Entity<Post>()
                 .HasOne(p => p.Topic)
