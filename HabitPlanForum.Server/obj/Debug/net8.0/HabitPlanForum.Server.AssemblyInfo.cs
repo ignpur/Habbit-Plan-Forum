3aeb67b6443d699c2e50d5304a5d7cc28f8cdbd3
IgnPur <100840309+ignpur@users.noreply.github.com>
2024-12-16 20:58:02 +0200

    WORKINGFINNALY

--- HabitPlanForum.Server/obj/Debug/net8.0/HabitPlanForum.Server.AssemblyInfo.cs
+++ HabitPlanForum.Server/obj/Debug/net8.0/HabitPlanForum.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitPlanForum.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d723af47cdf23da73eb5e4bd1e1c6171fffc31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d250fb29944fbd294e8cb414c2fef354db4e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitPlanForum.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitPlanForum.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
